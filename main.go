76ec34ca6c33f042e38d5496a58874264f59c7e0
Ivor Thibodo <ivorisoutdoors@users.noreply.github.com>
2026-02-22 21:08:21 -0600

    feat(log): switch to slog and add verbose logging

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -19,6 +19,7 @@ var (
 	file     string
 	interval time.Duration
 	once     bool
+	verbose  bool
 	client   = http.DefaultClient
 )
 
@@ -26,30 +27,39 @@ func main() {
 	flag.StringVar(&user, "user", "", "Username to fetch keys for")
 	flag.StringVar(&url, "url", "https://github.com", "Base URL to fetch keys from")
 	flag.StringVar(&file, "file", "~/.ssh/authorized_keys", "File to update with keys (replaces content)")
-	flag.BoolVar(&once, "once", false, "Run once and then exit")
 	flag.DurationVar(&interval, "interval", time.Minute, "update interval (ex. 5s, 1m, 3h)")
+	flag.BoolVar(&once, "once", false, "Run once and then exit")
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose logging")
 	flag.Parse()
 
+	if verbose {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
 	if user == "" {
-		log.Fatal("missing -user")
+		slog.Error("missing -user")
+		os.Exit(1)
 	}
 
 	if file[0] == '~' {
 		home, err := os.UserHomeDir()
 		if err != nil {
-			log.Fatal("Could not determine home directory")
+			slog.Error("Could not determine home directory", slog.Any("error", err))
+			os.Exit(1)
 		}
 		file = filepath.Join(home, file[1:])
 	}
 
 	if err := run(context.Background()); err != nil {
-		log.Fatal(err)
+		slog.Error("Could not run ssh-key-sync", slog.Any("error", err))
+		os.Exit(1)
 	}
 }
 
 func run(ctx context.Context) error {
+	slog.Debug("Running initial sync")
 	if err := sync(ctx); err != nil {
-		return fmt.Errorf("could sync: %w", err)
+		return fmt.Errorf("could run initial sync: %w", err)
 	}
 
 	if once {
@@ -60,13 +70,16 @@ func run(ctx context.Context) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
+	slog.Debug("Starting ticker", slog.Duration("interval", interval))
 	for {
 		select {
 		case <-ctx.Done():
+			slog.Info("Stopping sync")
 			return nil
 		case <-ticker.C:
+			slog.Debug("Running sync")
 			if err := sync(ctx); err != nil {
-				log.Print(err.Error())
+				slog.Error("Could not sync", slog.Any("error", err))
 			}
 		}
 	}
@@ -78,6 +91,7 @@ func sync(ctx context.Context) error {
 		return fmt.Errorf("could not build request: %w", err)
 	}
 
+	slog.Debug("Fetching keys", slog.String("url", req.URL.String()))
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("could not fetch keys: %w", err)
@@ -89,6 +103,7 @@ func sync(ctx context.Context) error {
 		return fmt.Errorf("could not read keys: %w", err)
 	}
 
+	slog.Debug("Updating key file", slog.String("file", file))
 	if err := os.WriteFile(file, keys, 0600); err != nil {
 		return fmt.Errorf("could not write file: %w", err)
 	}
